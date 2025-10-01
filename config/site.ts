14e2409ce187f987f6a351494c6340c998e575ff
Roy Duenas <roy.duenas.sdtpnoli@gmail.com>
2025-10-02 02:39:05 +0800

    Updated page title

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Juliane Faith Cuizon",
+  description: "A passionate newcomer fueled by creativity and a relentless drive to learn and explore.",
   navItems: [
     {
       label: "Home",
